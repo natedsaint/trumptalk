858113b632a18ba35ea07c4cb79615c6a20c538b
Nathan St. Pierre <natedsaint@gmail.com>
2017-01-27 01:43:53 +0000

    first go

---
+++ app.js
@@ -0,0 +1,24 @@
+var express = require('express')
+var app = express()
+var maba = require('maba');
+var bot = new maba();
+ 
+app.get('/ask', function (req, res) {
+  var answer;
+  var error;
+  if (req.query.q) {
+    answer = bot.ask(req.query.q);
+  } else {
+    error = "Sad! Don't understand this"; 
+  }
+  if (error) {
+    res.status(400).json({error});
+  } else {
+    res.json({answer});
+  }
+  
+});
+
+app.use('/',express.static('public'));
+ 
+app.listen(3000)
